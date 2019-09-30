018f73b3bb9a439308c74ae5f08cb431c9675e6c
hsosa1966 <47432735+hsosa1966@users.noreply.github.com>
2019-09-30 17:28:59 -0400

    Add files via upload

---
+++ Get-VCSAsettings.ps1
@@ -0,0 +1,23 @@
+﻿
+$ntpTime = (Get-CisService -Name 'com.vmware.appliance.techpreview.ntp').get()
+$hostname = (Get-CisService -Name 'com.vmware.appliance.networking.dns.hostname').get()
+$Time = (Get-CisService -Name 'com.vmware.appliance.system.time').get()
+$DNS = (Get-CisService -Name 'com.vmware.appliance.networking.dns.servers').get()
+$Health = (Get-CisService 'com.vmware.appliance.health.system').get()
+$SystemVersion = (Get-CisService -Name 'com.vmware.appliance.system.version').get()
+
+write-host
+
+write-host "HostName: " $hostname
+Write-Host "Product: " $SystemVersion.summary
+Write-Host "Type: " $SystemVersion.type
+Write-Host "Version: " $SystemVersion.version
+Write-Host "Build: " $SystemVersion.build
+Write-Host "Install Time: " $SystemVersion.install_time
+Write-Host "Overall Health: " $Health
+write-host "NTP Servers: " $ntpTime.servers
+write-host "NTP Status: " $ntpTime.status
+write-host "TimeZone: " $time.timezone
+Write-Host "Date: " $Time.date
+write-host "Current Time: " $time.time
+Write-Host "DNS Servers: " $DNS.servers
